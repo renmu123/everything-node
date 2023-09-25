0499ad1aad46ed4739735ac92fdba195818667b4
renmu123 <1101022351@qq.com>
2023-09-25 16:57:39 +0800

    complete part

--- README-ZH.md
+++ README-ZH.md
@@ -3,8 +3,96 @@
 这个一个 everything CLI 的包装项目
 
 es.exe version 1.1.0.26
-everything version >1.4
+everything version 1.4.x
 
 # 警告
 
-请勿添加如 `-size-format` 的格式化参数，否则会破坏格式化代码运行，那你只能自行格式化了
+请勿添加如 `-size-format` 等格式化参数，否则会破坏格式化代码运行，那你只能自行格式化了
+
+# 示例
+
+## 简单搜索示例
+
+```js
+const everything = new Everything();
+everything.limit(10);
+everything.addSearchText("内容");
+const data = await everything.run();
+console.log(data);
+```
+
+## 展示最大的十个文件
+
+```js
+const everything = new Everything();
+everything.limit(10);
+everything.sortBy("size");
+const data = await everything.run();
+console.log(data);
+```
+
+# API
+
+## addSearchText(text, options): 添加搜索参数
+
+```ts
+interface SearchOptions {
+  matchCase?: boolean; // 区分大小写
+  regex?: boolean; // 正则
+  wholeWord?: boolean; // 全字匹配
+  matchPath?: boolean; // 路径匹配
+  folderOnly?: boolean; // 仅有文件夹
+  fileOnly?: boolean; // 仅有文件
+  diacritics?: boolean; // 匹配重音标识
+}
+```
+
+## limit(num): 添加限制
+
+## offset(num): 添加偏移
+
+## sortBy(valus): 排序
+
+`"name"|"path"|"size"|"extension"|"date"|"date-created"|"date-modified"|"date-accessed"|"attributes" |"date-recently-changed"|"run-count"|"date-recently-changed"|"date-run"|"ascending"|"descending";`
+
+## getTotal(): 获取搜索的总条数
+
+## addOptions(args): 添加原始参数
+
+`addOptions("-whole-words")`
+
+## promise run(): 运行搜索命令
+
+返回值
+
+```ts
+interface Result {
+  type: "dir" | "file";
+  path: string;
+  lastModified: string;
+  size?: number;
+}
+[];
+```
+
+## setExecPath(path): 设置可执行文件路径，不使用默认 es.exe
+
+## generateOptions(): 获取搜索时传递给命令行的参数
+
+## esVersion(): 获取 es.exe 的版本
+
+## version(): 获取 everything 版本
+
+# Error Code
+
+| Errorlevel | Description                                                                                |
+| ---------- | ------------------------------------------------------------------------------------------ |
+| 0          | No known error, search successful.                                                         |
+| 1          | Failed to register window class.                                                           |
+| 2          | Failed to create listening window.                                                         |
+| 3          | Out of memory.                                                                             |
+| 4          | Expected an additional command line option with the specified switch.                      |
+| 5          | Failed to create export output file.                                                       |
+| 6          | Unknown switch.                                                                            |
+| 7          | Failed to send Everything IPC a query.                                                     |
+| 8          | Everything IPC window not found. Please make sure the Everything search client is running. |
--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ interface Result {
   type: "dir" | "file";
   path: string;
   lastModified: string;
-  size?: string;
+  size?: number;
 }
 
 type SortValue =
@@ -29,14 +29,54 @@ type SortValue =
   | "ascending"
   | "descending";
 
+interface SearchOptions {
+  matchCase?: boolean;
+  regex?: boolean;
+  wholeWord?: boolean;
+  matchPath?: boolean;
+  folderOnly?: boolean;
+  fileOnly?: boolean;
+  diacritics?: boolean;
+}
+
 export default class Everything {
-  readonly options: string[] = ["-size", "-dm", "-date-format 1"];
-  private searchText: string = "";
+  readonly rawOptions: string[] = [
+    "-size",
+    "-dm",
+    "-date-format 1",
+    "-no-digit-grouping",
+  ];
+  readonly options: {
+    limit?: number;
+    offset?: number;
+    sortBy?: SortValue[];
+    searchText?: string;
+    searchOptions?: SearchOptions;
+  } = {
+    searchText: "",
+    sortBy: [],
+  };
   rawData: string[] = [];
+  execPath: string = path.join(__dirname, "bin", "es.exe");
 
   constructor() {}
-  addOptions(vaule: string) {
-    this.options.push(vaule);
+  addOptions(value: string) {
+    // if (value.startsWith("-sort")) {
+    //   console.warn("If you want to sort, use the sortBy method.");
+    // } else if (value.startsWith("-n") || value.startsWith("-max-result")) {
+    //   console.warn("If you want to add limit, use the limit method.");
+    // } else if (value.startsWith("-o") || value.startsWith("-offset")) {
+    //   console.warn("If you want to add offset, use the offset method.");
+    // } else if (value.startsWith("-r")) {
+    //   console.warn(
+    //     "If you want to add search text, use the addSearchText method."
+    //   );
+    // } else if (value.startsWith("-regex")) {
+    //   console.warn(
+    //     "If you want to add search text, use the addSearchText method."
+    //   );
+    // }
+    this.rawOptions.push(value);
   }
 
   parse(items: string[]): Result[] {
@@ -60,7 +100,7 @@ export default class Everything {
           type: "file",
           path: match[3] ?? "",
           lastModified: match[2] ?? "",
-          size: (match[1] ?? "").replaceAll(",", ""),
+          size: Number(match[1]) ?? 0,
         };
 
         result.push(item2);
@@ -69,7 +109,8 @@ export default class Everything {
     return result;
   }
   async run(): Promise<Result[]> {
-    let data = await this.spawnChild([...this.options, this.searchText]);
+    const options = this.generateOptions();
+    let data = await this.spawnChild(options);
 
     this.rawData = data
       .split("\r\n")
@@ -81,56 +122,117 @@ export default class Everything {
   }
 
   private async spawnChild(options?: string[]) {
-    const execPath = path.join(__dirname, "bin", "es.exe");
+    return new Promise<string>(async (resolve, reject) => {
+      const child = spawn(this.execPath, options, {
+        shell: true,
+      });
 
-    const child = spawn(execPath, options, {
-      shell: true,
+      let data = "";
+      for await (const chunk of child.stdout) {
+        data += chunk;
+      }
+      let error = "";
+      for await (const chunk of child.stderr) {
+        error += chunk;
+      }
+      const exitCode = await new Promise((resolve, reject) => {
+        child.on("close", resolve);
+      });
+
+      if (exitCode) {
+        reject(`${error}`);
+      }
+      resolve(data);
     });
-
-    let data = "";
-    for await (const chunk of child.stdout) {
-      data += chunk;
-    }
-    let error = "";
-    for await (const chunk of child.stderr) {
-      error += chunk;
-    }
-    const exitCode = await new Promise((resolve, reject) => {
-      child.on("close", resolve);
-    });
-
-    if (exitCode) {
-      throw new Error(`${error}`);
-    }
-    return data;
   }
-  addSearchText(text: string) {
-    this.searchText = text;
+  addSearchText(text: string, options: SearchOptions = {}) {
+    const searchOptions = {
+      matchCase: false,
+      regex: false,
+      wholeWord: false,
+      matchPath: false,
+      folderOnly: false,
+      fileOnly: false,
+      diacritics: false,
+      ...options,
+    };
+    this.options.searchOptions = searchOptions;
+    this.options.searchText = text;
     return this;
   }
-  addLimit(size: Number) {
-    this.options.push(`-n ${size}`);
+  limit(size: number) {
+    this.options.limit = size;
+    return this;
+  }
+  offset(size: number) {
+    this.options.offset = size;
     return this;
   }
   sortBy(values: SortValue[]) {
-    values.forEach(value => {
-      this.options.push(`-sort-${value}`);
-    });
+    this.options.sortBy = values;
   }
   async esVersion() {
     let version = await this.spawnChild(["-version"]);
     return version;
   }
-  async everythingVersion() {
+  async version() {
     let version = await this.spawnChild(["-get-everything-version"]);
     return version;
   }
   async getTotal() {
-    let count = await this.spawnChild([
-      ...this.options,
-      "-get-result-count",
-      this.searchText,
-    ]);
+    const options = this.generateOptions();
+    let count = await this.spawnChild([...options, "-get-result-count"]);
     return Number(count);
   }
+  generateOptions() {
+    const options = [];
+    if (this.options.limit) {
+      options.push(`-n ${this.options.limit}`);
+    }
+    if (this.options.offset) {
+      options.push(`-offset ${this.options.offset}`);
+    }
+    if (this.options.sortBy) {
+      this.options.sortBy.forEach(value => {
+        options.push(`-sort-${value}`);
+      });
+    }
+
+    if (this.options.searchText) {
+      if (this.options.searchOptions?.folderOnly) {
+        options.push("-ad");
+      }
+      if (this.options.searchOptions?.fileOnly) {
+        options.push("-a-d");
+      }
+      if (this.options.searchOptions?.diacritics) {
+        options.push("-diacritics");
+      }
+      if (this.options.searchOptions?.matchCase) {
+        options.push("-case");
+      }
+      if (this.options.searchOptions?.wholeWord) {
+        options.push("-whole-word");
+      }
+      if (this.options.searchOptions?.matchPath) {
+        options.push("-match-path");
+      }
+
+      if (this.options.searchOptions?.regex) {
+        options.push(`-regex "${escaped(this.options.searchText)}"`);
+      } else {
+        options.push(`-r ${escaped(this.options.searchText)}`);
+      }
+    }
+    return [...this.rawOptions, ...options];
+  }
+  setExecPath(path: string) {
+    this.execPath = path;
+    return this;
+  }
 }
+
+// Use ^ to escape \, &, |, >, < and ^.
+export const escaped = (s: string) => {
+  return s.replaceAll(/([\\&|><^])/g, "^$1");
+};
