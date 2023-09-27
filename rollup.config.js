e682cfb707133765b00b1cb53eb071c47d815f1b
renmu123 <1101022351@qq.com>
2023-09-27 10:34:37 +0800

    export esm & cjs and add index.d.ts

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,39 @@
 import typescript from "@rollup/plugin-typescript";
 import copy from "rollup-plugin-copy";
-
+import dts from "rollup-plugin-dts";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 
-export default {
-  input: "src/index.ts",
-  output: {
-    dir: "dist",
-    sourcemap: "inline",
-    format: "es",
-    exports: "named",
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: "dist/index.esm.js",
+        sourcemap: "inline",
+        format: "es",
+        exports: "named",
+      },
+      {
+        file: "dist/index.cjs.js",
+        sourcemap: "inline",
+        format: "cjs",
+        exports: "named",
+      },
+    ],
+    plugins: [
+      typescript(),
+      nodeResolve({ browser: false }),
+      copy({
+        targets: [{ src: "src/bin/es.exe", dest: "dist/bin" }],
+      }),
+    ],
   },
-  plugins: [
-    typescript(),
-    nodeResolve({ browser: false }),
-    copy({
-      targets: [{ src: "src/bin/es.exe", dest: "dist/bin" }],
-    }),
-  ],
-};
+  {
+    input: "src/index.ts",
+    plugins: [dts()],
+    output: {
+      format: "esm",
+      file: "dist/index.d.ts",
+    },
+  },
+];
